376b94193ff4af1f00b3f6baeb1460b77a8f67d4
minshaf998 <minshaf998@gmail.com>
2022-09-26 01:57:55 +0530

    Updates

---
+++ precache-manifest.7ac27ae9cc5d5c3ffa1568585e2caef1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf50c5b343f9a96d602056f2d35e1148",
+    "url": "/myweb/index.html"
+  },
+  {
+    "revision": "e4302b25c0d60f20ea31",
+    "url": "/myweb/static/css/main.a48af857.chunk.css"
+  },
+  {
+    "revision": "b11216b04132fa6e7b4b",
+    "url": "/myweb/static/js/2.aab4ccda.chunk.js"
+  },
+  {
+    "revision": "37fdc2d2e258a4a0ff71fb7bf84cf091",
+    "url": "/myweb/static/js/2.aab4ccda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4302b25c0d60f20ea31",
+    "url": "/myweb/static/js/main.ba8a7449.chunk.js"
+  },
+  {
+    "revision": "a7ecc25430e9168e6ddb",
+    "url": "/myweb/static/js/runtime-main.ee8fccef.js"
+  },
+  {
+    "revision": "418b0aa275bed12b084b274929ed2ae6",
+    "url": "/myweb/static/media/header__bg.418b0aa2.png"
+  }
+]);
